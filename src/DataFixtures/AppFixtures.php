4a9270e18e1113aa26798e2790a595c9fc476fb2
Guillaume <guillaumebaillieux88@gmail.com>
2024-10-28 11:34:13 +0100

    add Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        # Instanciation d'un User
+        $user = new User();
+        
+        $user->setUniqid('1');
+        $user->setUsername('admin');
+        $user->setEmail('admin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN','ROLE_REDAC','ROLE_MODERATOR']);
+        $user->setPassword('admin');
+        $user->setActivate(true);
+        $user->setFullname('The Admin !');
 
+        # Utilisation du $manager pour mettre le
+        # User en mémoire
+        $manager->persist($user);
+
+        # envoie à la base de donnée (commit)
         $manager->flush();
     }
 }
